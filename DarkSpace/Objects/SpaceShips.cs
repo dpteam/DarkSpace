257338d04c2e5d4820dce204d493f109a37f2a9c
DartPower <SIMPSON-NEKEK@mail.ru>
2020-06-23 22:00:46 +0300

    Небольшая цаца

---
+++ DarkSpace/Objects/SpaceShips.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DarkSpace.Objects
+{
+    public class SpaceShips
+    {
+        // Informatic
+        public string ShipName = null;
+
+        // Psychic
+        public int Health = 0;
+        public int maxHealth = 0;
+
+        public int Shield = 0;
+        public int maxShield = 0;
+
+        public bool isLive = true;
+        public bool shieldActive = true;
+
+        public float Speed = 0f;
+
+        // Economic
+        public bool Wanted = false;
+
+        //TODO: Move to player...
+        //public int penalty;
+    }
+}
---
+++ DarkSpace/ProgramClient.cs
@@ -0,0 +1,96 @@
+﻿using SpeechLib;
+using System;
+
+namespace DarkSpace
+{
+    internal class ProgramClient
+    {
+        public static void MainRun()
+        {
+            System.Runtime.InteropServices.Marshal.PrelinkAll(typeof(Program));
+            Console.BackgroundColor = ConsoleColor.Black;
+            SpVoice sp2 = new SpVoice();
+
+            /*
+            string[] gangNames = { "Alpha", "Beta", "Omega" };
+
+            foreach (string gangNameElement in gangNames) {
+                Console.WriteLine("Gang: " + gangNameElement);
+            }
+            */
+
+            //PrintStringFatalError("Kernel Panic");
+            //PrintStringFatalError("0xFFFFFFFF: No CRC[Code-Reproduction-Code]");
+
+            bool __KERNEL_DEBUG_MODE = true;
+            if (__KERNEL_DEBUG_MODE == true)
+            {
+                //sp2.Speak(Local.strDbgModeOn, SpeechVoiceSpeakFlags.SVSFlagsAsync);
+                Print("[INFO]" + Local.strDbgModeOn);
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("____________________________________________________________________________________________");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("   /$$$$$$$                      /$$        /$$$$$$                                         ");
+            Console.WriteLine("  | $$__  $$                    | $$       /$$__  $$                                        ");
+            Console.WriteLine("  | $$  \\ $$  /$$$$$$   /$$$$$$ | $$   /$$| $$  \\__/  /$$$$$$   /$$$$$$   /$$$$$$$  /$$$$$$ ");
+            Console.WriteLine("  | $$  | $$ |____  $$ /$$__  $$| $$  /$$/|  $$$$$$  /$$__  $$ |____  $$ /$$_____/ /$$__  $$");
+            Console.WriteLine("  | $$  | $$  /$$$$$$$| $$  \\__/| $$$$$$/  \\____  $$| $$  \\ $$  /$$$$$$$| $$      | $$$$$$$$");
+            Console.WriteLine("  | $$  | $$ /$$__  $$| $$      | $$_  $$  /$$  \\ $$| $$  | $$ /$$__  $$| $$      | $$_____/");
+            Console.WriteLine("  | $$$$$$$/|  $$$$$$$| $$      | $$ \\  $$|  $$$$$$/| $$$$$$$/|  $$$$$$$|  $$$$$$$|  $$$$$$$");
+            Console.WriteLine("  |_______/  \\_______/|__/      |__/  \\__/ \\______/ | $$____/  \\_______/ \\_______/ \\_______/");
+            Console.WriteLine("                                                    | $$                                    ");
+            Console.WriteLine("                                                    | $$                                    ");
+            Console.WriteLine("                                                    |__/                                    ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("____________________________________________________________________________________________");
+
+            // Seperator
+            Console.WriteLine("");
+
+            // Welcome
+            sp2.Speak(Local.strWelcome, SpeechVoiceSpeakFlags.SVSFlagsAsync);
+            Console.WriteLine(Local.strWelcome);
+
+            // Seperator
+            Console.WriteLine("");
+
+            // Selector
+            int menuSelectorNum;
+            Console.WriteLine(Local.strSelector);
+            Console.WriteLine(Local.strSelectorNewGame);
+            Console.WriteLine(Local.strSelectorExit);
+            menuSelectorNum = Convert.ToInt32(Console.ReadLine());
+
+            if (menuSelectorNum == 1)
+            {
+                Console.Clear();
+                Console.WriteLine("Вы начали новую игру");
+            }
+            if (menuSelectorNum == 2)
+            {
+                System.Diagnostics.Process.GetCurrentProcess().Kill();
+            }
+            else
+            {
+                PrintStringFatalError(Local.strUnknownFatalError);
+            }
+
+            Console.ReadKey();
+        }
+
+        public static void Print(string __string)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(__string);
+        }
+
+        public static void PrintStringFatalError(string str)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("[FATAL] " + str);
+        }
+
+    }
+}
