a3c1bab986d7b334cdaf01a5a618d73b8cae0bf6
DartPower <SIMPSON-NEKEK@mail.ru>
2020-10-25 02:48:11 +0300

    Create CheatSystem.cs

---
+++ DarkSpace/Config/CheatSystem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DarkSpace.Config
+{
+    class CheatSystem
+    {
+        // dQw4w9WgXcQ
+        // MISSINGNO
+        // KONAMI-CODE // да
+    }
+}
